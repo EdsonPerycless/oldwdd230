f454ef691700d982601c577e880355509522febb
EdsonPerycless <edsonperyclessud@gmail.com>
2021-09-26 01:06:17 -0300

    origin

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
+//("Last Updated " +  now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear() )
 let getDate = new Date
-
 window.onload = function myFunction() {
     document.getElementById("p1").innerHTML = getDate;
 }
